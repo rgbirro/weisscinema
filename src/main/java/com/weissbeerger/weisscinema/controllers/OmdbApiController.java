f70eb7c2f2ea486cba280fdc5fa6e3435785cc2a
rgbirro <rgbirro@gmail.com>
2022-05-07 20:50:00 -0300

    Initial commit

---
+++ src/main/java/com/weissbeerger/weisscinema/controllers/OmdbApiController.java
@@ -0,0 +1,15 @@
+package com.weissbeerger.weisscinema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class OmdbApiController {
+
+    @RequestMapping("/movies")
+    public String getMoovies() {
+
+        return "movies";
+    }
+}
